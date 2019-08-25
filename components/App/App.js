0089a5c85af7af0be358621df6b1233cdd7a363f
Eddie Tindame <eddietindame@eddies-mbp.mynet>
2019-08-25 20:00:27 +0100

    Initial commit

---
+++ components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.scss'
+
+const App = () =>
+    <div className="app">
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <h1>Hello, World!</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda hic, aspernatur explicabo reiciendis maiores tenetur suscipit dignissimos unde. Commodi optio minus reiciendis ipsa tempore nihil illum recusandae enim rerum doloremque?</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+export default App
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import App from '~components/App'
+import '~scss/index.scss'
+
+const Index = () =>
+    <div
+        id="index"
+        className="index"
+    >
+        <App />
+    </div>
+
+export default Index
